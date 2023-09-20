0ac9444e5d562e93b8a4bff1a64489171bf69470
eun0leee <eun0leee@gmail.com>
2023-09-20 23:00:07 +0900

    fix : 모바일 width 값 고정 (#2)

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet, ScrollRestoration } from 'react-router-dom';
 const Layout = () => {
   return (
     <>
-      <div className="h-[100vh] min-w-[373px] bg-yellow-50">
+      <div className="h-[100vh] w-[375px] bg-yellow-50">
         <main className="h-full">
           <Outlet />
         </main>
