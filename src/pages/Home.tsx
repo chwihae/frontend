269c6ac25aa46f6dec982a1007a7dc757747c790
eun0leee <eun0leee@gmail.com>
2023-09-20 22:52:42 +0900

    feat : 질문작성 페이지 이동 버튼 (#5)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,16 @@
+import { Link } from 'react-router-dom';
+
 import Level from '@components/Home/Level';
 
 const Home = () => {
   return (
-    <div className="h-full">
+    <div className="relative h-full">
       <Level />
+      <Link to="/question">
+        <button className="btn absolute bottom-[41px] right-4 h-12 w-12 rounded-full border-0 bg-orange-500 p-0 hover:bg-orange-500">
+          작성
+        </button>
+      </Link>
     </div>
   );
 };
