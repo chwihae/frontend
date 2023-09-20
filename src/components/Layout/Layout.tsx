0df191549c3957951a0258a408bac674462500f8
eun0leee <eun0leee@gmail.com>
2023-09-20 17:02:37 +0900

    fix : 공통과 비공개 레이아웃 main height 수정 (#2)

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -18,7 +18,7 @@ const Layout = () => {
             </header>
           ),
         )}
-        <main>
+        <main className="h-full">
           <Outlet />
         </main>
       </div>
--- src/components/Layout/PrivateLayout.tsx
+++ src/components/Layout/PrivateLayout.tsx
@@ -2,7 +2,11 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 const PrivateLayout = () => {
   const isLogin = !!localStorage.getItem('accessToken');
-  return <>{isLogin ? <Outlet /> : <Navigate to="/intro" />}</>;
+  return (
+    <div className="h-[calc(100vh-60px)]">
+      {isLogin ? <Outlet /> : <Navigate to="/intro" />}
+    </div>
+  );
 };
 
 export default PrivateLayout;
