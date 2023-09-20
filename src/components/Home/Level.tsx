2cfc668497bbb863e4282f62a380ce99cf26fabd
eun0leee <eun0leee@gmail.com>
2023-09-20 20:22:00 +0900

    feat : 헤더, 레벨 컴포넌트 마크업 (#5)

--- src/components/Home/Level.tsx
+++ src/components/Home/Level.tsx
@@ -1,37 +1,12 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-
-import { getUserInfo } from '@/apis/auth';
-import { ReactComponent as IConLevelFour } from '@/assets/levelfour.svg';
-import { ReactComponent as IConLevelOne } from '@/assets/levelone.svg';
-import { ReactComponent as IConLevelThree } from '@/assets/levelthree.svg';
-import { ReactComponent as IConLevelTwo } from '@/assets/leveltwo.svg';
-import type { IUser } from '@/types/authType';
+import useUserInfo from '@/hooks/useUserInfo';
+import LevelImage from '@components/common/LevelImage';
 
 const Level = () => {
-  const [user, setUser] = useState<IUser>();
-  const accessToken = localStorage.getItem('accessToken');
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getUserInfo(accessToken);
-      setUser(data?.data);
-    };
-    fetchData();
-  }, [accessToken]);
-
-  const LEVEL = [
-    { level: '학사', img: <IConLevelOne className="h-24 w-24" /> },
-    { level: '석사', img: <IConLevelTwo className="h-24 w-24" /> },
-    { level: '박사', img: <IConLevelThree className="h-24 w-24" /> },
-    { level: '교사', img: <IConLevelFour className="h-24 w-24" /> },
-  ];
+  const user = useUserInfo();
 
   return (
     <section className="flex h-1/3 flex-col items-center justify-center gap-4">
-      <Link className="h-24 w-24" to="/mypage">
-        {LEVEL.find((item) => item.level === user?.level)?.img}
-      </Link>
+      <LevelImage className="h-24 w-24" />
       <p className="text-xl">나는 {user?.level} 별랑이!</p>
       <div className="flex w-full justify-center gap-2">
         <span className="bg-orange-400">아이콘</span>
--- src/components/Layout/HeaderHome.tsx
+++ src/components/Layout/HeaderHome.tsx
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import ModalPreparing from '@components/common/ModalPreparing';
+
 const HeaderHome = () => {
-  return <div>HeaderHome</div>;
+  const [isModal, setIsModal] = useState(false);
+
+  return (
+    <>
+      <h1 className="absolute">CHWIHAE</h1>
+      <div className="absolute right-2 flex text-sm font-bold">
+        <label
+          htmlFor="save-modal"
+          className="btn border-0"
+          onClick={() => setIsModal(true)}
+        >
+          검색
+        </label>
+        <Link className="flex items-center" to="/mypage">
+          플필
+        </Link>
+      </div>
+      {isModal && <ModalPreparing name="save-modal" />}
+    </>
+  );
 };
 
 export default HeaderHome;
