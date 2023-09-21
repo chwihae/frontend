a6cb8f69a6acc59cecb8b6c16d32306896c79c25
eun0leee <eun0leee@gmail.com>
2023-09-21 16:55:41 +0900

    fix : 플로팅버튼 위치 조정 (#5)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,7 +11,7 @@ const Home = () => {
   const [solvedIndex, setSolvedIndex] = useState(0);
 
   return (
-    <div className="relative h-full">
+    <>
       <Level />
       <TabBar
         tabIndex={tabIndex}
@@ -24,11 +24,11 @@ const Home = () => {
       />
       <VoteList tabIndex={tabIndex} solvedIndex={solvedIndex} />
       <Link to="/question">
-        <button className="btn absolute bottom-[41px] right-4 h-12 w-12 rounded-full border-0 bg-orange-500 p-0 hover:bg-orange-500">
+        <button className="btn absolute bottom-[41px] right-[16px] h-12 w-12 rounded-full border-0 bg-orange-500 p-0 hover:bg-orange-500">
           작성
         </button>
       </Link>
-    </div>
+    </>
   );
 };
 
