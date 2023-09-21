d240dcfd529563881c79807a2d1feb856f3c733d
eun0leee <eun0leee@gmail.com>
2023-09-21 15:19:08 +0900

    feat : voteFilterBtn 디자인 (#5)

--- src/components/Home/VoteFilterBtn.tsx
+++ src/components/Home/VoteFilterBtn.tsx
@@ -1,3 +1,4 @@
+import { RADIOOPTIONS } from '@/constants/home';
 type TVoteFilterBtn = {
   solvedIndex: number;
   setSolvedIndex: (arg: number) => void;
@@ -10,14 +11,22 @@ const VoteFilterBtn = ({ solvedIndex, setSolvedIndex }: TVoteFilterBtn) => {
 
   return (
     <div className="px-4 pt-4">
-      {radioOptions.map((option) => (
-        <label key={option.value}>
+      {RADIOOPTIONS.map((option) => (
+        <label
+          key={option.value}
+          className={`w-full rounded-[37px] py-2 ${
+            solvedIndex === option.value
+              ? ' bg-prime1 text-white'
+              : 'border-[1px] border-GS6 text-GS4'
+          }`}
+        >
           <input
             type="radio"
             name="voteFilter"
             value={option.value}
             checked={solvedIndex === option.value}
             onChange={handleRadioChange}
+            className="hidden"
           />
           {option.text}
         </label>
@@ -27,9 +36,3 @@ const VoteFilterBtn = ({ solvedIndex, setSolvedIndex }: TVoteFilterBtn) => {
 };
 
 export default VoteFilterBtn;
-
-const radioOptions = [
-  { text: '전체', value: 0 },
-  { text: '해결중', value: 1 },
-  { text: '해결완료', value: 2 },
-];
