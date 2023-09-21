f109ec9f45c562b579aa9d409c1fe2da146d791d
eun0leee <eun0leee@gmail.com>
2023-09-21 17:21:24 +0900

    feat : 데스크톱 바탕색, 쉐도우 적용 (#5)

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,14 +2,14 @@ import { Outlet, ScrollRestoration } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <>
-      <div className="relative mx-auto h-[100vh] max-h-[926px] w-full min-w-[375px] max-w-[428px] overflow-y-hidden bg-yellow-50">
+    <div>
+      <div className="overflow-y-hidde relative mx-auto h-[100vh] max-h-[926px] w-full min-w-[375px] max-w-[428px] bg-white shadow-lg">
         <div className="h-full">
           <Outlet />
         </div>
       </div>
       <ScrollRestoration />
-    </>
+    </div>
   );
 };
 
