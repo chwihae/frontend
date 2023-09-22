7a0d7fc8c8839d52237f087e5547ae8419f4ed76
eun0leee <eun0leee@gmail.com>
2023-09-23 05:07:24 +0900

    feat : 홈 등급 value 조회 (#5#6)

--- src/components/Home/Level.tsx
+++ src/components/Home/Level.tsx
@@ -1,20 +1,55 @@
-import useUserInfo from '@/hooks/useUserInfo';
+import { useEffect } from 'react';
+
+import { getUserLevel } from '@/apis/auth';
+import { ReactComponent as IConCommentOrange } from '@/assets/icon_comment_orange.svg';
+import { ReactComponent as IConVoteOrange } from '@/assets/icon_vote_orange.svg';
+import { LEVELSTEP } from '@/constants/home';
+import getLocalData from '@/utils/getLocalData';
 import LevelImage from '@components/common/LevelImage';
 
 const Level = () => {
-  const user = useUserInfo();
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getUserLevel();
+      return res;
+    };
+    fetchData();
+  }, []);
+
+  // 등급 이름
+  const userLevel = getLocalData('userLevel');
+  const userLevelName = LEVELSTEP.find(
+    (level) => level.type === userLevel?.level,
+  );
+
+  const turnout =
+    userLevelName &&
+    Math.round((userLevel?.voteCount / userLevelName?.goalVotes) * 100);
+  const commentRate =
+    userLevelName &&
+    Math.round((userLevel?.commentCount / userLevelName?.goalComments) * 100);
 
   return (
-    <section className="flex h-1/3 flex-col items-center justify-center gap-4 pt-[34px]">
-      <LevelImage className="h-24 w-24" />
-      <p className="text-xl">나는 {user?.level} 별랑이!</p>
-      <div className="flex w-full justify-center gap-2">
-        <span className="bg-orange-400">아이콘</span>
-        <div className="h-6 w-4/6 bg-orange-400">투표 프로그레스바</div>
-      </div>
-      <div className="flex w-full justify-center gap-2">
-        <span className="bg-orange-400">아이콘</span>
-        <div className="h-6 w-4/6 bg-orange-400">댓글 프로그레스바</div>
+    <section className="relative flex h-[229px] flex-col">
+      <LevelImage className="absolute right-[21.56px] top-[44.32px] h-[145px] w-[155px]" />
+      <div className="ml-[16px] mt-[41px] w-[180px]">
+        <p className="scorebold24 right-0 flex flex-col text-right">
+          <span>나의 등급은</span>
+          <span className="mb-[15px] translate-y-[-5px] text-prime1">
+            {userLevelName && userLevelName?.name} 별랑이!
+          </span>
+          <span className="scoreregular12 mb-2 text-GS2 ">다음 등급까지</span>
+        </p>
+        <div className="scoremedium12 w-full text-GS3">
+          <div className="flex">
+            <IConVoteOrange />
+            <span>{turnout}%</span>
+          </div>
+          <div className="flex">
+            <IConCommentOrange />
+            <span>{commentRate}%</span>
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/common/LevelImage.tsx
+++ src/components/common/LevelImage.tsx
@@ -1,37 +1,35 @@
-import { Link } from 'react-router-dom';
-
 import { ReactComponent as IConLevelOne } from '@/assets/char_level1.svg';
 import { ReactComponent as IConLevelTwo } from '@/assets/char_level2.svg';
 import { ReactComponent as IConLevelThree } from '@/assets/char_level3.svg';
 import { ReactComponent as IConLevelFour } from '@/assets/char_level4.svg';
-import useUserInfo from '@/hooks/useUserInfo';
+import getLocalData from '@/utils/getLocalData';
 
 const LevelImage = ({ className }: { className: string }) => {
-  const user = useUserInfo();
+  const userLevel = getLocalData('userLevel');
 
   const LEVEL = [
     {
-      level: '학사',
+      level: 'BACHELOR',
       img: <IConLevelOne className={`${className}`} />,
     },
     {
-      level: '석사',
+      level: 'MASTER',
       img: <IConLevelTwo className={`${className}`} />,
     },
     {
-      level: '박사',
+      level: 'DOCTOR',
       img: <IConLevelThree className={`${className}`} />,
     },
     {
-      level: '교사',
+      level: 'PROFESSOR',
       img: <IConLevelFour className={`${className}`} />,
     },
   ];
 
   return (
-    <Link className="flex items-center" to="/mypage">
-      {LEVEL.find((item) => item.level === user?.level)?.img}
-    </Link>
+    <span className="flex items-center">
+      {LEVEL.find((item) => item.level === userLevel?.level)?.img}
+    </span>
   );
 };
 
