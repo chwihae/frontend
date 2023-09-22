d7fbc91b620fe5b667959d6fc343f1ccc525134c
eun0leee <eun0leee@gmail.com>
2023-09-22 23:47:07 +0900

    feat : 준비중 모달 디자인 (#6)

--- src/components/common/ModalPreparing.tsx
+++ src/components/common/ModalPreparing.tsx
@@ -1,13 +1,23 @@
+import { ReactComponent as IConWarning } from '@/assets/icon_warning.svg';
+
 const ModalPreparing = ({ name }: { name: string }) => {
   return (
     <>
       <input type="checkbox" id={name} className="modal-toggle" />
       <div className="modal">
-        <div className="flex flex-col items-center gap-4 rounded-xl bg-white p-8">
-          <h4 className="font-bold">해당 기능은 준비중이예요.</h4>
-          <p className="">열심히 개발하겠습니다! 😀</p>
-          <div className="modal-action">
-            <label htmlFor={name} className="btn">
+        <div className="flex h-[250px] w-[250px] flex-col items-center gap-3 rounded-[20px] bg-white py-[27px]">
+          <IConWarning />
+          <h4 className="scorebold16">해당 기능은 준비중이에요</h4>
+          <p className="notosansregular14 text-center">
+            열심히 개발중이니
+            <br />
+            조금만 더 기다려 주세요!
+          </p>
+          <div className="modal-action mt-3">
+            <label
+              htmlFor={name}
+              className="scoremedium14 btn m-0 h-[42px] w-[95px] rounded-[10px] bg-prime1 py-[9px] text-center text-white hover:bg-prime1"
+            >
               알겠어요
             </label>
           </div>
