1f8e5df8fd8be944528c6a33b335ad8a12f1da14
eun0leee <eun0leee@gmail.com>
2023-09-23 04:33:36 +0900

    feat : 레벨스텝 constants 추가 (#5)

--- src/constants/home.ts
+++ src/constants/home.ts
@@ -11,3 +11,10 @@ export const RADIOOPTIONS = [
   { text: '해결중', value: 1, status: 'IN_PROGRESS' },
   { text: '해결완료', value: 2, status: 'COMPLETED' },
 ];
+
+export const LEVELSTEP = [
+  { name: '학사', type: 'BACHELOR', goalVotes: 20, goalComments: 5 },
+  { name: '석사', type: 'MASTER', goalVotes: 100, goalComments: 30 },
+  { name: '박사', type: 'DOCTOR', goalVotes: 300, goalComments: 100 },
+  { name: '교수', level: 'PROFESSOR', goalVotes: 0, goalComments: 0 },
+];
