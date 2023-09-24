bc9eb291c235b0392bf362c9d1670b5fb70dcb6a
eun0leee <eun0leee@gmail.com>
2023-09-24 12:31:24 +0900

    chore : tanstack query 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+const queryClient = new QueryClient({
+  //   defaultOptions: {
+  //     queries: { suspense: true, retry: false },
+  //   },
+});
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+    <ReactQueryDevtools />
+  </QueryClientProvider>,
+);
