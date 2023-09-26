73c7cf34e49a8d155e0a8cf2df814a7efd5715cf
eun0leee <eun0leee@gmail.com>
2023-09-26 09:52:04 +0900

    feat : 고민기간 툴팁 제작 (#8)

---
+++ src/components/Question/TooltipPeriod.tsx
@@ -0,0 +1,12 @@
+import { TOOLTIP_PERIOD } from '@/constants/question';
+
+const TooltipPeriod = () => {
+  return (
+    <div className="notosansmedium12 absolute left-[-91px] top-[-90px] flex w-[200px] flex-col items-center justify-center whitespace-pre-wrap rounded-[10px] bg-prime1 px-4 py-[10px] text-left text-white ">
+      {TOOLTIP_PERIOD}
+      <div className="absolute bottom-[-26px] h-0 w-0 border-b-[18px] border-l-[10px] border-r-[10px] border-t-[18px] border-b-transparent border-l-transparent border-r-transparent border-t-prime1"></div>
+    </div>
+  );
+};
+
+export default TooltipPeriod;
