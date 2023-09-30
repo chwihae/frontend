d95ce444e4f173530db2b48994751608b31be8c0
eun0leee <eun0leee@gmail.com>
2023-09-30 15:38:27 +0900

    fix : 모바일 100vh 대응 (#29)
    - body 에 100vh 추가

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <div className="hide-scroll relative mx-auto h-screen max-h-[926px] w-[375px] max-w-[428px] overflow-scroll bg-white shadow-lg">
+    <div className="relative mx-auto h-screen max-h-[926px] w-[375px] max-w-[428px] bg-white shadow-lg">
       <div className="h-full">
         <Outlet />
       </div>
