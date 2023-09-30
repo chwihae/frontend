2e4aecf5496413bb62c9b7d933db0d5215c68a2a
eun0leee <eun0leee@gmail.com>
2023-09-30 15:48:14 +0900

    fix : 모바일 100hv 대응 (#29)

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <div className="relative mx-auto h-screen max-h-[926px] w-[375px] max-w-[428px] overflow-hidden bg-white shadow-lg">
+    <div className="hide-scroll relative mx-auto h-screen max-h-[926px] w-[375px] max-w-[428px] overflow-scroll bg-white shadow-lg">
       <div className="h-full">
         <Outlet />
       </div>
