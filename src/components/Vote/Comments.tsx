d4f4101239e563b1049d1e736ac1998ecf711ad2
eun0leee <eun0leee@gmail.com>
2023-11-01 14:54:56 +0900

    feat : 댓글 무한스크롤 구현 (#19)

--- src/components/Vote/Comments.tsx
+++ src/components/Vote/Comments.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 
+import { getComment } from '@/apis/vote';
 import { ReactComponent as IConKebabGray } from '@/assets/icon_kebab_gray.svg';
 import { ReactComponent as IConSendGray } from '@/assets/icon_send_gray.svg';
 import { ReactComponent as IConSendOrange } from '@/assets/icon_send_orange.svg';
 import { useIsBottomSheetContext } from '@/contexts/IsBottomSheetProvider';
 import useAddCommentMutation from '@/hooks/comment/useAddCommentMutation';
-import useGetCommentQuery from '@/hooks/comment/useGetCommentQuery';
+import type { ICommentGetRes } from '@/types/voteType';
 import BottomSheet from '@components/common/BottomSheet';
 import Toast from '@components/common/Toast';
 
@@ -13,7 +16,6 @@ const Comments = ({ postId }: { postId: number }) => {
   //댓글
   const [inputComment, setInputComment] = useState('');
   const [commentId, setCommentId] = useState(0);
-  const commentList = useGetCommentQuery(postId);
   const addCommentMutate = useAddCommentMutation();
   const [toastDeleteComment, setToastDeleteComment] = useState<boolean>(false);
 
@@ -31,8 +33,6 @@ const Comments = ({ postId }: { postId: number }) => {
     }
   };
 
-  console.log(commentList);
-
   // 댓글 삭제
   const { setIsBottomSheetOpen } = useIsBottomSheetContext();
   const handleKebabBtn = (id: number) => {
@@ -41,40 +41,72 @@ const Comments = ({ postId }: { postId: number }) => {
   };
   const { isBottomSheetOpen } = useIsBottomSheetContext();
 
+  // 무한스크롤
+  const [ref, inView] = useInView();
+
+  const { data, hasNextPage, fetchNextPage } = useInfiniteQuery(
+    ['comments', postId],
+    ({ pageParam = 0 }) => getComment({ questionId: postId, page: pageParam }),
+    {
+      getNextPageParam: (lastPage) => {
+        if (!lastPage.last) return lastPage.number + 1;
+      },
+      select: (newData) => ({
+        pages: newData.pages,
+        pageParams: newData.pages[0].number,
+      }),
+    },
+  );
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inView, hasNextPage]);
+
   return (
     <>
       <section className="px-4 pb-[52px] pt-12">
-        <h3 className="scorebold16 mb-6">댓글 {commentList?.length}</h3>
+        <h3 className="scorebold16 mb-6">
+          댓글 {data?.pages[0].totalElements}
+        </h3>
         {/* 댓글목록 */}
         <ol className="grid gap-3">
-          {commentList?.length !== 0 ? (
-            commentList
-              ?.sort((a, b) => a.id - b.id)
-              .map((comment) => (
-                <li
-                  key={comment?.id}
-                  className="border-b-[1px] border-b-bg pb-3"
-                >
-                  <div className="notosansmedium16 mb-3 flex h-[26px] items-center justify-between">
-                    <span>{comment?.commenterAlias}</span>
-                    {comment.editable && (
-                      <label
-                        htmlFor="bottomSheet-commentEdit-modal"
-                        className="scoremedium16 flex cursor-pointer items-center justify-between"
-                        onClick={() => handleKebabBtn(comment?.id)}
-                      >
-                        <IConKebabGray />
-                      </label>
-                    )}
-                  </div>
-                  <p className="notosansregular16 mb-1 text-GS2">
-                    {comment?.content}
-                  </p>
-                  <span className="notosansregular12 text-GS4">
-                    {comment?.createdAt.replaceAll('-', '.').replace('T', '. ')}
-                  </span>
-                </li>
-              ))
+          {data && data?.pages[0].totalElements !== 0 ? (
+            <>
+              {data.pages.map((page) => (
+                <div key={page.number}>
+                  {page.content.map((comment: ICommentGetRes) => (
+                    <div key={comment?.id}>
+                      <li className="border-b-[1px] border-b-bg pb-3">
+                        <div className="notosansmedium16 mb-3 flex h-[26px] items-center justify-between">
+                          <span>{comment?.commenterAlias}</span>
+                          {comment.editable && (
+                            <label
+                              htmlFor="bottomSheet-commentEdit-modal"
+                              className="scoremedium16 flex cursor-pointer items-center justify-between"
+                              onClick={() => handleKebabBtn(comment?.id)}
+                            >
+                              <IConKebabGray />
+                            </label>
+                          )}
+                        </div>
+                        <p className="notosansregular16 mb-1 text-GS2">
+                          {comment?.content}
+                        </p>
+                        <span className="notosansregular12 text-GS4">
+                          {comment?.createdAt
+                            .replaceAll('-', '.')
+                            .replace('T', '. ')}
+                        </span>
+                      </li>
+                      <div ref={ref} className="h-1"></div>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </>
           ) : (
             <p className="scoremedium12 my-16 text-center text-GS4">
               아직 댓글이 없어요
