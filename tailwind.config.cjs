96c37f238ec3266811c9c22a2f959a418eeb7e41
eun0leee <eun0leee@gmail.com>
2023-11-03 01:08:38 +0900

    chore : tailwlincss.config.cjs 에 디자인시스템 폰트 적용

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     fontFamily: {
-      sscore: ['S-CoreDream-3Light'],
+      score: ['S-CoreDream-3Light'],
       notosanskr: ['Noto Sans KR', 'sans-serif'],
     },
     extend: {
@@ -21,8 +22,157 @@ module.exports = {
       },
     },
   },
-  plugins: [require('daisyui')],
   daisyui: {
     themes: [],
   },
+  plugins: [
+    require('daisyui'),
+    ({ addUtilities }) => {
+      addUtilities({
+        /* S-CoreDream-3Light */
+        '.score-intro': {
+          fontFamily: 'S-CoreDream-3Light',
+          fontSize: '32px',
+          fontStyle: 'normal',
+          fontWeight: 200,
+          lineHeight: 'normal',
+          letterSpacing: '0.64px',
+        },
+        '.score-bold24': {
+          fontFamily: 'S-CoreDream-3Light',
+          fontSize: '24px',
+          fontWeight: 600,
+          lineHeight: 1.6,
+          letterSpacing: '0.48px',
+        },
+        '.score-bold20': {
+          fontFamily: 'S-CoreDream-3Light',
+          fontSize: '20px',
+          fontWeight: 600,
+          lineHeight: 1.6,
+          letterSpacing: '0.4px',
+        },
+        '.score-bold18': {
+          fontFamily: 'S-CoreDream-3Light',
+          fontSize: '18px',
+          fontWeight: 600,
+          lineHeight: 1.6,
+          letterSpacing: '0.36px',
+        },
+        '.score-bold16': {
+          fontFamily: 'S-CoreDream-3Light',
+          fontSize: '16px',
+          fontWeight: 600,
+          lineHeight: '26px',
+          letterSpacing: '0.02em',
+        },
+        '.score-medium20': {
+          fontFamily: 'S-CoreDream-3Light',
+          fontSize: '20px',
+          fontStyle: 'normal',
+          fontWeight: 500,
+          lineHeight: 1.6,
+          letterSpacing: '0.4px',
+        },
+        '.score-medium16': {
+          fontFamily: 'S-CoreDream-3Light',
+          fontSize: '16px',
+          fontStyle: 'normal',
+          fontWeight: 500,
+          lineHeight: 1.6,
+          letterSpacing: '0.32px',
+        },
+        '.score-medium14': {
+          fontFamily: 'S-CoreDream-3Light',
+          fontSize: '14px',
+          fontStyle: 'normal',
+          fontWeight: 500,
+          lineHeight: 1.6,
+          letterSpacing: '0.28px',
+        },
+        '.score-medium12': {
+          fontFamily: 'S-CoreDream-3Light',
+          fontSize: '12px',
+          fontStyle: 'normal',
+          fontWeight: 500,
+          lineHeight: 'normal',
+          letterSpacing: '0.24px',
+        },
+        '.score-regular16': {
+          fontFamily: 'S-CoreDream-3Light',
+          fontSize: '16px',
+          fontStyle: 'normal',
+          fontWeight: 200,
+          lineHeight: 1.6,
+          letterSpacing: '0.32px',
+        },
+        '.score-regular12': {
+          fontFamily: 'S-CoreDream-3Light',
+          fontSize: '12px',
+          fontStyle: 'normal',
+          fontWeight: 200,
+          lineHeight: 'normal',
+          letterSpacing: '0.6px',
+        },
+
+        /* Noto Sans KR' */
+        '.notosans-bold16': {
+          fontFamily: 'Noto Sans KR',
+          fontSize: '16px',
+          fontStyle: 'normal',
+          fontWeight: 700,
+          lineHeight: 1.6,
+          letterSpacing: '0.32px',
+        },
+        '.notosans-medium16': {
+          fontFamily: 'Noto Sans KR',
+          fontSize: '16px',
+          fontStyle: 'normal',
+          fontWeight: 500,
+          lineHeight: 1.6,
+          letterSpacing: '0.32px',
+        },
+        '.notosans-medium14': {
+          fontFamily: 'Noto Sans KR',
+          fontSize: '14px',
+          fontStyle: 'normal',
+          fontWeight: 500,
+          lineHeight: 1.6,
+          letterSpacing: '0.28px',
+        },
+        '.notosans-medium12': {
+          fontFamily: 'Noto Sans KR',
+          fontSize: '12px',
+          fontStyle: 'normal',
+          fontWeight: 500,
+          lineHeight: 1.6,
+          letterSpacing: '0.24px',
+        },
+        '.notosans-regular16': {
+          fontFamily: 'Noto Sans KR',
+          fontSize: '16px',
+          fontStyle: 'normal',
+          fontWeight: 400,
+          lineHeight: 1.6,
+          letterSpacing: '0.32px',
+        },
+        '.notosans-regular14': {
+          fontFamily: 'Noto Sans KR',
+          fontSize: '14px',
+          fontStyle: 'normal',
+          fontWeight: 400,
+          lineHeight: 1.6,
+          letterSpacing: '0.28px',
+        },
+        '.notosans-regular12': {
+          fontFamily: 'Noto Sans KR',
+          fontSize: '12px',
+          fontStyle: 'normal',
+          fontWeight: 400,
+          lineHeight: 1.6,
+          letterSpacing: '0.24px',
+        },
+      });
+    },
+  ],
 };
