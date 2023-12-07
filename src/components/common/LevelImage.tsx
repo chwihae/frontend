982eb01279eeda87393c38220516e91db1cfa052
eun0leee <eun0leee@gmail.com>
2023-12-07 18:55:23 +0900

    fix : 유저 등급에 따른 이미지에 원형 추가 (#19)

--- src/components/common/LevelImage.tsx
+++ src/components/common/LevelImage.tsx
@@ -1,32 +1,51 @@
+import { useLocation } from 'react-router-dom';
+
 import { ReactComponent as IConLevelOne } from '@/assets/char_level1.svg';
+import { ReactComponent as IConLevelOneRoundedLarge } from '@/assets/char_level1_round_100.svg';
 import { ReactComponent as IConLevelTwo } from '@/assets/char_level2.svg';
+import { ReactComponent as IConLevelTwoRoundedLarge } from '@/assets/char_level2_round_100.svg';
 import { ReactComponent as IConLevelThree } from '@/assets/char_level3.svg';
+import { ReactComponent as IConLevelThreeRoundedLarge } from '@/assets/char_level3_round_100.svg';
 import { ReactComponent as IConLevelFour } from '@/assets/char_level4.svg';
+import { ReactComponent as IConLevelFourRoundedLarge } from '@/assets/char_level4_round_100.svg';
 import getLocalData from '@/utils/getLocalData';
 
 const LevelImage = ({ className }: { className: string }) => {
+  const location = useLocation();
+  const { pathname } = location;
+
   const userLevel = getLocalData('userLevel');
 
   const LEVEL = [
     {
       type: 'BACHELOR',
-      img: <IConLevelOne className={`${className}`} />,
+      mainImg: <IConLevelOne className={`${className}`} />,
+      mypageImg: <IConLevelOneRoundedLarge className={`${className}`} />,
     },
     {
       type: 'MASTER',
-      img: <IConLevelTwo className={`${className}`} />,
+      mainImg: <IConLevelTwo className={`${className}`} />,
+      mypageImg: <IConLevelTwoRoundedLarge className={`${className}`} />,
     },
     {
       type: 'DOCTOR',
-      img: <IConLevelThree className={`${className}`} />,
+      mainImg: <IConLevelThree className={`${className}`} />,
+      mypageImg: <IConLevelThreeRoundedLarge className={`${className}`} />,
     },
     {
       type: 'PROFESSOR',
-      img: <IConLevelFour className={`${className}`} />,
+      mainImg: <IConLevelFour className={`${className}`} />,
+      mypageImg: <IConLevelFourRoundedLarge className={`${className}`} />,
     },
   ];
 
-  return <>{LEVEL.find((item) => item.type === userLevel?.type)?.img}</>;
+  const filteredUserLevel = LEVEL.find((item) => item.type === userLevel?.type);
+  const userLevelImage =
+    pathname === '/home'
+      ? filteredUserLevel?.mainImg
+      : filteredUserLevel?.mypageImg;
+
+  return <>{userLevelImage}</>;
 };
 
 export default LevelImage;
